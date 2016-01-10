eccad5ed485497207526c5911b55aa359827f7e9
Lock <gbyukg@163.com>
2016-01-10 13:02:17 +0800

    Time update

--- src/time/time.h
+++ src/time/time.h
@@ -103,3 +103,73 @@ time(time_t *timep);
  */
 char *
 ctime(const time_t *timep);
+
+/**
+ * @brief 将日历时间转换为分解时间 @ref tm
+ *
+ * 该函数返回的结果是经静态分配的, 因此主要后续操作对该结构体的更改以及其他函数调用对它的影响.
+ *
+ * @param timep 要转换的日历时间
+ *
+ * @return 返回一个经<B>静态</B>分配的 @ref tm 结构
+ * @retval NULL 函数执行失败
+ */
+struct tm *
+gmtime(const time_t *timep);
+
+/**
+ * @brief 将日历时间转换为分解时间 @ref tm
+ *
+ * 该函数在转换过程中同时考虑本地系统中的时区和夏令时设置.<BR>
+ * 该函数返回的结果是经静态分配的, 因此主要后续操作对该结构体的更改以及其他函数调用对它的影响.
+ *
+ * @param timep 要转换的日历时间
+ *
+ * @return 返回一个经<B>静态</B>分配的 @ref tm 结构
+ * @retval NULL 函数执行失败
+ */
+struct tm *
+localtime(const time_t *timep);
+
+/**
+ * @brief 将分解时间转换成日历时间
+ *
+ * 该函数可能会修改 @p timeptr 所指向的结构体, 至少会确保对 tm_wday 和 tm_day
+ * 字段的设置, 会与其他输入字段的值对应起来.<BR>
+ * 同时 @p timeptr 中的任意字段的值超出了范围, 都会对其进行相应的修正.
+ *
+ * @param timeptr 指向 @ref tm 结构的指针
+ *
+ * @return 返回获取到的日历时间
+ * @retval -1 函数执行失败
+ */
+time_t
+mktime(struct tm *timeptr);
+
+/**
+ * @brief 将分解时间转换成与 ctime() 返回结构一样的打印时间
+ *
+ * @param timeptr 执行 @ref tm 的结构的指针
+ *
+ * @return 返回一个经由静态分配的字符串指针
+ * @retval NULL 函数执行失败.
+ */
+char *
+asctime(const struct tm *timeptr);
+
+/**
+ * @brief 将分解时间转换成可打印的字符串
+ *
+ * 该函数提供了更加精确的控制来制定日期字符串输出的样式
+ *
+ * @param outstr 保存函数返回的日期字符串
+ * @param maxsize 制定了 @p outstr 的最大长度
+ * @param format 如何格式化日期字符串
+ * @param timeptr 指向一个 @ref tm 的分解时间结构体指针
+ *
+ * @return 返回在 @p outstr 中替换掉的字符串个数
+ * @retval 0 函数执行失败
+ */
+size_t
+strftime(char *outstr, size_t maxsize, const char *format,
+        const struct tm *timeptr);
